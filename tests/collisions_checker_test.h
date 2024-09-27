f6241be1e3e2d11cbb751246f193a9bdaa14523b
Evangelina Marj <evangelm@pr-c6.kzn.21-school.ru>
2024-09-27 17:35:27 +0300

    develop-tests-tetris. Rewrite collision_checker_test

--- tests/collisions_checker_test.h
+++ tests/collisions_checker_test.h
@@ -8,198 +8,196 @@
 #endif
 
 START_TEST(CheckCollideTest1) {
-  Player player = {0};
-  Board board = {0};
-  InitPlayer(&player);
-  InitPlayerPosition(&player);
+  Player* player = AllocPlayer();
+  Board *board = AllocBoard();
+  InitPlayer(player);
+  InitPlayerPosition(player);
 
-  InitBoard(&board);
+  InitBoard(board);
 
-  SetPlayerBlockType(&player, kBlockT);
-  SetPlayerBlockRotation(&player, kBlockRotationFirst);
-  bool flag = CheckCollide(&player, &board);
+  SetPlayerBlockType(player, kBlockT);
+  SetPlayerBlockRotation(player, kDirectionFirst);
+  bool flag = CheckCollide(player, board);
   ck_assert_int_eq(flag, false);
 }
-
 END_TEST
 
 START_TEST(CheckFutureCollideWithBlocksLeftTest1) {
-  Player player = {0};
-  Board board = {0};
-  InitPlayer(&player);
-  InitPlayerPosition(&player);
+  Player* player = {0};
+  Board *board = {0};
+  InitPlayer(player);
+  InitPlayerPosition(player);
 
-  InitBoard(&board);
+  InitBoard(board);
 
-  SetPlayerBlockType(&player, kBlockT);
-  SetPlayerBlockRotation(&player, kBlockRotationFirst);
-  bool flag = CheckFutureCollideWithBlocksLeft(&player, &board);
+  SetPlayerBlockType(player, kBlockT);
+  SetPlayerBlockRotation(player, kDirectionFirst);
+  bool flag = CheckFutureCollideWithBlocksLeft(player, board);
   ck_assert_int_eq(flag, false);
 }
-
 END_TEST
 
 START_TEST(CheckFutureCollideWithBlocksRightTest1) {
-  Player player = {0};
-  Board board = {0};
-  InitPlayer(&player);
-  InitPlayerPosition(&player);
+  Player* player = AllocPlayer();
+  Board *board = AllocBoard();
+  InitPlayer(player);
+  InitPlayerPosition(player);
 
-  InitBoard(&board);
-  SetPlayerBlockType(&player, kBlockT);
-  SetPlayerBlockRotation(&player, kBlockRotationFirst);
-  bool flag = CheckFutureCollideWithBlocksRight(&player, &board);
+  InitBoard(board);
+  SetPlayerBlockType(player, kBlockT);
+  SetPlayerBlockRotation(player, kDirectionFirst);
+  bool flag = CheckFutureCollideWithBlocksRight(player, board);
   ck_assert_int_eq(flag, false);
 }
 
 END_TEST
 
 START_TEST(CheckFutureCollideWithBlocksLeftTest2) {
-  Player player = {0};
-  Board board = {0};
-  InitPlayer(&player);
-  InitPlayerPosition(&player);
+  Player *player = AllocPlayer();
+  Board *board = AllocBoard();
+  InitPlayer(player);
+  InitPlayerPosition(player);
 
-  InitBoard(&board);
-  for (int i = 0; i < board.height_; ++i) {
-    board.cells_[i][0].is_set_ = true;
+  InitBoard(board);
+  for (int i = 0; i < board->height_; ++i) {
+    board->cells_[i][0].is_set_ = true;
   }
 
-  SetPlayerBlockType(&player, kBlockT);
-  SetPlayerBlockRotation(&player, kBlockRotationFirst);
+  SetPlayerBlockType(player, kBlockT);
+  SetPlayerBlockRotation(player, kDirectionFirst);
 
-  MovePlayerLeft(&player);
-  MovePlayerLeft(&player);
-  MovePlayerLeft(&player);
-  bool flag = CheckFutureCollideWithBlocksLeft(&player, &board);
+  MovePlayerLeft(player);
+  MovePlayerLeft(player);
+  MovePlayerLeft(player);
+  bool flag = CheckFutureCollideWithBlocksLeft(player, board);
   ck_assert_int_eq(flag, true);
 }
 
 END_TEST
 START_TEST(CheckFutureCollideWithBlocksRightTest2) {
-  Player player = {0};
-  Board board = {0};
-  InitPlayer(&player);
-  InitPlayerPosition(&player);
+  Player *player = AllocPlayer();
+  Board *board = AllocBoard();
+  InitPlayer(player);
+  InitPlayerPosition(player);
 
-  InitBoard(&board);
-  for (int i = 0; i < board.height_; ++i) {
-    board.cells_[i][board.width_ - 1].is_set_ = true;
+  InitBoard(board);
+  for (int i = 0; i < board->height_; ++i) {
+    board->cells_[i][board->width_ - 1].is_set_ = true;
   }
 
-  SetPlayerBlockType(&player, kBlockT);
-  SetPlayerBlockRotation(&player, kBlockRotationFirst);
+  SetPlayerBlockType(player, kBlockT);
+  SetPlayerBlockRotation(player, kDirectionFirst);
 
-  MovePlayerRight(&player);
-  MovePlayerRight(&player);
-  bool flag = CheckFutureCollideWithBlocksRight(&player, &board);
+  MovePlayerRight(player);
+  MovePlayerRight(player);
+  bool flag = CheckFutureCollideWithBlocksRight(player, board);
   ck_assert_int_eq(flag, true);
 }
 
 END_TEST
 
 START_TEST(CheckFutureCollideWithLeftTest) {
-  Player player = {0};
-  Board board = {0};
-  InitPlayer(&player);
-  InitPlayerPosition(&player);
+  Player *player = AllocPlayer();
+  Board *board = AllocBoard();
+  InitPlayer(player);
+  InitPlayerPosition(player);
 
-  InitBoard(&board);
+  InitBoard(board);
 
-  SetPlayerBlockType(&player, kBlockT);
-  SetPlayerBlockRotation(&player, kBlockRotationFirst);
-  MovePlayerLeft(&player);
-  MovePlayerLeft(&player);
-  MovePlayerLeft(&player);
-  MovePlayerLeft(&player);
+  SetPlayerBlockType(player, kBlockT);
+  SetPlayerBlockRotation(player, kDirectionFirst);
+  MovePlayerLeft(player);
+  MovePlayerLeft(player);
+  MovePlayerLeft(player);
+  MovePlayerLeft(player);
 
-  bool flag = CheckFutureCollideWithLeft(&player, &board);
+  bool flag = CheckFutureCollideWithLeft(player, board);
   ck_assert_int_eq(flag, true);
 }
 
 END_TEST
 
 START_TEST(CheckFutureCollideWithRightTest) {
-  Player player = {0};
-  Board board = {0};
-  InitPlayer(&player);
-  InitPlayerPosition(&player);
+  Player *player = AllocPlayer();
+  Board *board = AllocBoard();
+  InitPlayer(player);
+  InitPlayerPosition(player);
 
-  InitBoard(&board);
+  InitBoard(board);
 
-  SetPlayerBlockType(&player, kBlockT);
-  SetPlayerBlockRotation(&player, kBlockRotationFirst);
-  MovePlayerRight(&player);
-  MovePlayerRight(&player);
-  MovePlayerRight(&player);
+  SetPlayerBlockType(player, kBlockT);
+  SetPlayerBlockRotation(player, kDirectionFirst);
+  MovePlayerRight(player);
+  MovePlayerRight(player);
+  MovePlayerRight(player);
 
-  bool flag = CheckFutureCollideWithRight(&player, &board);
+  bool flag = CheckFutureCollideWithRight(player, board);
   ck_assert_int_eq(flag, true);
 }
 
 END_TEST
 
 START_TEST(CheckFutureCollideWithUpTest) {
-  Player player = {0};
-  Board board = {0};
-  InitPlayer(&player);
-  InitPlayerPosition(&player);
+  Player *player = AllocPlayer();
+  Board *board = AllocBoard();
+  InitPlayer(player);
+  InitPlayerPosition(player);
 
-  InitBoard(&board);
+  InitBoard(board);
 
-  SetPlayerBlockType(&player, kBlockT);
-  SetPlayerBlockRotation(&player, kBlockRotationFirst);
-  MovePlayerRight(&player);
-  MovePlayerRight(&player);
-  MovePlayerRight(&player);
+  SetPlayerBlockType(player, kBlockT);
+  SetPlayerBlockRotation(player, kDirectionFirst);
+  MovePlayerRight(player);
+  MovePlayerRight(player);
+  MovePlayerRight(player);
 
-  bool flag = CheckFutureCollideWithUp(&player, &board);
+  bool flag = CheckFutureCollideWithUp(player, board);
   ck_assert_int_eq(flag, false);
 }
 
 END_TEST
 
 START_TEST(CheckFutureCollideWithDownTest) {
-  Player player = {0};
-  Board board = {0};
-  InitPlayer(&player);
-  InitPlayerPosition(&player);
+  Player *player = AllocPlayer();
+  Board *board = AllocBoard();
+  InitPlayer(player);
+  InitPlayerPosition(player);
 
-  InitBoard(&board);
+  InitBoard(board);
 
-  SetPlayerBlockType(&player, kBlockT);
-  SetPlayerBlockRotation(&player, kBlockRotationFirst);
-  MovePlayerRight(&player);
-  MovePlayerRight(&player);
-  MovePlayerRight(&player);
+  SetPlayerBlockType(player, kBlockT);
+  SetPlayerBlockRotation(player, kDirectionFirst);
+  MovePlayerRight(player);
+  MovePlayerRight(player);
+  MovePlayerRight(player);
 
-  bool flag = CheckFutureCollideWithDown(&player, &board);
+  bool flag = CheckFutureCollideWithDown(player, board);
   ck_assert_int_eq(flag, false);
-  MovePlayerDown(&player);
-  MovePlayerDown(&player);
-  MovePlayerDown(&player);
-  MovePlayerDown(&player);
-  MovePlayerDown(&player);
-  flag = CheckFutureCollideWithDown(&player, &board);
+  MovePlayerDown(player);
+  MovePlayerDown(player);
+  MovePlayerDown(player);
+  MovePlayerDown(player);
+  MovePlayerDown(player);
+  flag = CheckFutureCollideWithDown(player, board);
   ck_assert_int_eq(flag, false);
-  MovePlayerDown(&player);
-  MovePlayerDown(&player);
-  MovePlayerDown(&player);
-  MovePlayerDown(&player);
-  MovePlayerDown(&player);
-  flag = CheckFutureCollideWithDown(&player, &board);
+  MovePlayerDown(player);
+  MovePlayerDown(player);
+  MovePlayerDown(player);
+  MovePlayerDown(player);
+  MovePlayerDown(player);
+  flag = CheckFutureCollideWithDown(player, board);
   ck_assert_int_eq(flag, false);
-  MovePlayerDown(&player);
-  MovePlayerDown(&player);
-  MovePlayerDown(&player);
-  MovePlayerDown(&player);
-  MovePlayerDown(&player);
-  flag = CheckFutureCollideWithDown(&player, &board);
+  MovePlayerDown(player);
+  MovePlayerDown(player);
+  MovePlayerDown(player);
+  MovePlayerDown(player);
+  MovePlayerDown(player);
+  flag = CheckFutureCollideWithDown(player, board);
   ck_assert_int_eq(flag, false);
-  MovePlayerDown(&player);
-  MovePlayerDown(&player);
-  MovePlayerDown(&player);
-  flag = CheckFutureCollideWithDown(&player, &board);
+  MovePlayerDown(player);
+  MovePlayerDown(player);
+  MovePlayerDown(player);
+  flag = CheckFutureCollideWithDown(player, board);
   ck_assert_int_eq(flag, true);
 }
 
